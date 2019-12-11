aeac2bca244558b8e0724a0c6c6303ff6d9ddb45
Joe Soares <error698@gmail.com>
2019-12-11 00:16:31 -0600

    update day 3 to use shared function

--- advent_of_code/day_03/intersect.py
+++ advent_of_code/day_03/intersect.py
@@ -1,10 +1,7 @@
 """logic for wire intersections"""
 from typing import Dict, List
 
-
-def manhattan_distance(coord_1: tuple, coord_2: tuple):
-    """find distance between two points"""
-    return abs(coord_1[0] - coord_2[0]) + abs(coord_1[1] - coord_2[1])
+from shared.distance import manhattan_distance
 
 
 def wire_coords(directions: List[str]) -> Dict[tuple, int]:
